e77c43a0753aaa0a0db0371ff4f5ad38488f36e8
Felipe Costa <felipe95176@gmail.com>
2024-05-30 16:19:56 -0300

    comportamento do botão, que faz aparecer um pequeno menu

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function myFunction(x) {
     x.classList.toggle("change");
+    var menu = document.querySelector('.menu');
+    if (x.classList.contains("change")) {
+        menu.style.left = '0';
+    } else {
+        menu.style.left = '-200px';
+    }
   }
