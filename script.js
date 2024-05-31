7412fe84e49e350920365f9607b803ff065af9cf
Felipe Costa <felipe95176@gmail.com>
2024-05-31 08:52:29 -0300

    Melhorei ainda mais o comportamento do botão e adicionei um overlay para servir de fundo, como uma sombra, ao pressionar o botão

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 function myFunction(x) {
     x.classList.toggle("change");
     var menu = document.querySelector('.menu');
+    var overlayForMenu = document.querySelector('.overlay_shadowForMenu');
     if (x.classList.contains("change")) {
         menu.style.left = '0';
+        overlayForMenu.style.opacity = 1;
     } else {
         menu.style.left = '-200px';
+        overlayForMenu.style.opacity = 0;
     }
   }
