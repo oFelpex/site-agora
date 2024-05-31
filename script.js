52b2f272fc5dcf98aab08c866df1811e49bf6e36
Felipe Costa <felipe95176@gmail.com>
2024-05-31 09:41:03 -0300

    fanzendo uns testes, por enquanto a transição do overlay vai ficar assim

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-function myFunction(x) {
-    x.classList.toggle("change");
+function myFunction(button) {
+    button.classList.toggle("change");
     var menu = document.querySelector('.menu');
     var overlayForMenu = document.querySelector('.overlay_shadowForMenu');
-    if (x.classList.contains("change")) {
+    if (button.classList.contains("change")) {
         menu.style.left = '0';
+        overlayForMenu.style.display = 'block';
         overlayForMenu.style.opacity = 1;
+        
     } else {
         menu.style.left = '-200px';
         overlayForMenu.style.opacity = 0;
+        overlayForMenu.style.display = 'none';
     }
+    overlayForMenu.addEventListener('click', function() {
+        var button = document.querySelector('.button_menu');
+        myFunction(button);
+    });
   }
